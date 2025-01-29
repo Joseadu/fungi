defb8d5fa50c28b338e6111feab4f6e4b5be7a89
Jose Diaz <joseantonio@mydance.zone>
2025-01-29 13:23:12 +0100

    home --> footer added

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MainNavComponent } from '../../shared/main-nav/main-nav.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    MainNavComponent
+    MainNavComponent,
+    FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
