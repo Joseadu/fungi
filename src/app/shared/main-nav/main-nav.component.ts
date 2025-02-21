db328f00fbec030a5c5bcb3b6307fbb7d0a97cf9
Jose Diaz <joseantonio@mydance.zone>
2025-02-21 16:54:01 +0100

    mobile menu styles and functionality

--- src/app/shared/main-nav/main-nav.component.ts
+++ src/app/shared/main-nav/main-nav.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './main-nav.component.html',
   styleUrl: './main-nav.component.css'
 })
 export class MainNavComponent {
 
+  toggledMenu: boolean = false;
+
+  toggleMenu() {
+    this.toggledMenu = !this.toggledMenu;
+  }
 }
