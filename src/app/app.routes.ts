3f00d1eda9a7908bfaab73b88233a1410424a777
Jose Diaz <joseantonio@mydance.zone>
2025-02-24 14:50:08 +0100

    app.routes --> mushroom guide route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { MushrromGuideComponent } from './features/mushrrom-guide/mushrrom-guide.component';
 
 export const routes: Routes = [
+    {
+        path: 'mushroom-guide',
+        component: MushrromGuideComponent
+    },
     {
         path: '',
         component: HomeComponent
