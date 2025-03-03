8541ebd8eee7a98494bd01a805c7fb5a28aced86
Jose Diaz <joseantonio@mydance.zone>
2025-03-03 13:10:04 +0100

    closing menu after clicking outside or link

--- src/app/shared/main-nav/main-nav.component.ts
+++ src/app/shared/main-nav/main-nav.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -13,10 +13,28 @@ import { RouterLink } from '@angular/router';
   styleUrl: './main-nav.component.css'
 })
 export class MainNavComponent {
+  constructor(
+    private elRef: ElementRef
+  ) {}
 
   toggledMenu: boolean = false;
 
   toggleMenu() {
     this.toggledMenu = !this.toggledMenu;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const isInsideMenu = this.elRef.nativeElement.contains(target);
+    const isNavLink = target.closest('a');
+
+    if (!target.closest('.nav')) {
+      this.toggledMenu = false;
+    }
+
+    if (!isInsideMenu || isNavLink) {
+      this.toggledMenu = false;
+    }
   }
 }
